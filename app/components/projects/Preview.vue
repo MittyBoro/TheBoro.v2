7018798c98f44dc2f45bdc7d662721ed2c11c74a
Dima Boro <iboro770@gmail.com>
2025-10-05 00:48:04 +0300

    [ update ] - preview sizes

--- app/components/projects/Preview.vue
+++ app/components/projects/Preview.vue
@@ -9,10 +9,16 @@ const { project } = defineProps<{ project: ProjectsCollectionItem }>()
     v-if="project.preview"
     class="relative mask-b-from-white mask-b-from-30% mask-b-to-black mask-luminance transition"
   >
-    <NuxtImg
+    <NuxtPicture
       :src="project.preview"
-      class="pointer-events-none w-full transition select-none"
-      :alt="`Image for ${project.title}`"
+      format="webp"
+      densities="x1 x2"
+      sizes="100vw md:390px"
+      :imgAttrs="{
+        loading: 'lazy',
+        class: 'pointer-events-none w-full transition select-none',
+        alt: `Image for ${project.title}`,
+      }"
     />
     <div class="absolute inset-0 bg-black/10 mix-blend-hue"></div>
     <div class="bg-primary/30 absolute inset-0 mix-blend-hue"></div>
